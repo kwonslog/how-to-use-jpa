e93a896b0404150d054d2bf97f865a8318615609
kwon <korpgm@gmail.com>
2024-05-18 09:04:59 +0900

    feat: 엔티티 클래스 추가

---
+++ entity/src/main/java/com/example/entity/Payment.java
@@ -0,0 +1,34 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * 엔티티를 작성할떄 기본적으로 있어야 하는 항목들에 대한 샘플 코드이다.
+ */
+@Entity
+@Table(name = "payment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "AMOUNT")
+    private BigDecimal amount;
+
+    @Column(name = "DATE")
+    private LocalDateTime date;
+
+    @Column(name = "METHOD")
+    private String method;
+}
